1995a9dcb7be282947283263c688e82482e056b9
sev <meyer483@uwm.edu>
2025-12-24 00:46:24 -0600

    completed code modified in order to scan a website that allows a port scan of its network

--- portscanner.py
+++ portscanner.py
@@ -6,3 +6,18 @@ target="scanme.nmap.org"
 print(f"Beginning scan on:{target}")
 
 
+start_time=time.time()
+
+for port in range(1,1024):
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.settimeout(0.5)
+
+    result =s.connect_ex((target,port))
+
+
+    if result==0:
+        print(f"Found Open Port:{port}")
+    
+    s.close()
+
+print(f"Scan Complete in {time.time()-start_time:.2f}s")
